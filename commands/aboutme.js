8ddcfff1c7024d632a1614c66762cc68e8f276c0
gm8686 <50534070+gm8686@users.noreply.github.com>
2022-07-06 00:13:03 -0700

    redundancy
    redundancy, reduce sentence

--- commands/aboutme.js
+++ commands/aboutme.js
@@ -18,7 +18,7 @@ class AboutMeCommand extends Command {
     async exec(message, args) {
         await this.client.settings.set(message.member.user.id, 'userInfo', args.information);
         if(args.information != null) {
-            return message.channel.send(success("you've successfully set your user profile about me to **" + args.information + "**.", message));
+            return message.channel.send(success("you've successfully set your about me to **" + args.information + "**.", message));
         } else {
             return message.channel.send(error("you need to pick a value.", message));
         }
