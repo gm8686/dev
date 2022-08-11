3dede409bccac8d5373ea7b26aadb520f800bbb6
gm <canolaoilboy@gmail.com>
2022-08-11 01:04:11 -0700

    fix dupe class bug

--- commands/addclass.js
+++ commands/addclass.js
@@ -19,23 +19,30 @@ class AddClassCommand extends Command {
     
     async exec(message, args) {
         var arr = await this.client.settings.get(message.member.user.id, "classes") || [];
-        console.log(arr);
         if(arr[0] == "none") {
             arr = [];
         }
         if(args.information != null) {
-
-            const result = actualClasses.findIndex( item =>  args.information.toLowerCase() === item.toLowerCase());
+            const result = actualClasses.findIndex( item =>  item.toLowerCase().includes(args.information.toLowerCase()));
             if(result == -1) {
-                return message.channel.send(error("that class isn't currently in the approved class database.", message));
+                return message.channel.send(error("that class isn't currently in the approved class database. Use $classes to see a list of approved classes.", message));
             } else {
+                for(var i = 0; i < arr.length; i++) {
+                    if(arr[i].toLowerCase().includes(args.information.toLowerCase())) {
+                        return message.channel.send(error("you already have that class.", message));
+                    }
+                }              
+                message.guild.channels.cache.forEach(ch => 
+                {
+                    if(ch.name == (actualClasses[result].replace(/\s/g, "-").toLowerCase()))
+                    ch.updateOverwrite(message.member.user.id, { VIEW_CHANNEL: true, SEND_MESSAGES: true });
+                }) 
                 arr.push(actualClasses[result]);
                 await this.client.settings.set(message.member.user.id, "classes", arr)
                 message.channel.send(success("you've successfully added **" + actualClasses[result] + "** to your class list.", message, true))
             }
-            console.log(result);
         } else {
-            message.channel.send(error("you need to specify a value.", message));
+            message.channel.send(error("you need to specify a value. Use $classes to see a list of approved classes.", message));
         }
     }
     
