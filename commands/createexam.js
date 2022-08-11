f20b48d92425e9f06bb6740f0d1fc72ccdb9eecc
gm <canolaoilboy@gmail.com>
2022-08-11 02:11:41 -0700

    fix bug

--- commands/createexam.js
+++ commands/createexam.js
@@ -17,16 +17,22 @@ class CreateExamCommand extends Command {
     }
 
     async exec(message, args) {
-
+        if(message.channel.parent != undefined) {
+            if(message.channel.parent.name.toLowerCase() != "classes") {
+                return message.channel.send({ embeds: [error("you can't use this command outside of a class channel.", message)] });
+            }
+        } else {
+            return message.channel.send({ embeds: [error("encountered an error finding channel parent.", message)] });
+        }
         if(args.name != null) {
+            var str = args.name[1] || "none";
             const thread = await message.channel.threads.create({
                 name: args.name[0],
-                autoArchiveDuration: 60,
-                reason: args.name[1] || "none",
+                autoArchiveDuration: 4320,
+                reason: str,
             });
             const chan = message.guild.channels.cache.get(thread.id);
-            var str = args.name[1] || "none";
-            const msg = await(chan.send({ embeds: [success("**Assignment Details: ** " + args.name[1] + "\n**Created By:** "+ message.member.user.tag, message, false)] }));
+            const msg = await(chan.send({ embeds: [success("**Assignment Details: ** " + str + "\n**Created By:** "+ message.member.user.tag, message, false)] }));
         } else {
             message.channel.send({ embeds: [error("you need to specify the name of the assignment.", message)] });
         }
